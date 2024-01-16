307771da8789988ac5a6f1edce6632492497e308
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-01-16 14:15:24 +0100

    Update forms.py

--- apps/forms.py
+++ apps/forms.py
@@ -1,31 +1,37 @@
 from django import forms
-from .models import User
-class UserForm(forms.ModelForm):
-    name_user = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control',}))
-    lastname_user = forms.CharField(label='Lastanme', widget=forms.TextInput(attrs={
-        'class':'form-control',
-    }))
-    GENRE_CHOICES = [
-        ('M','Masculin'),
-        ('F','Feminin'),
-        ('Autre','Autre'),
-    ]
-    sex_user  = forms.ChoiceField(label='sex', choices=GENRE_CHOICES ,widget=forms.TextInput(attrs={'class':'form-control',}))
-    phone_user = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'class':'form-control',}))
-    date_user = forms.DateField(label='Date ', widget=forms.DateInput(
-        attrs={'class':'form-control',}
-    ))
-    check_in_user = forms.TimeField(label='Check In', widget=forms.TimeInput(attrs={
-        'class':'form-control',
-    }))
-    check_out_user = forms.TimeField(label='Check In', widget=forms.TimeInput(attrs={
-        'class':'form-control',
-    }))
+from .models import PersonalOffice
+
+class PersonalForm(forms.ModelForm):
+
     class Meta:
-        model = User
-        fields =('name_user' , 'lastname_user', 'phone_user',
-                 'date_user', 'check_in_user','check_out_user' )
-        
-        
-        
+        model = PersonalOffice
+        fields = [
+            "fristname", 
+            "lastname",
+            "sex",
+            "phone",
+            "email"
+        ]
+        widgets = { 
+            'fristname': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'First Name',
+                'required': True, 
+                } ),
+            'lastname': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Last Name',
+                'required': True, 
+                } ),
+            'phone' : forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Phone',
+                'required': True,
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Addresse email',
+                'required': True, 
+                } )
+        }
     
