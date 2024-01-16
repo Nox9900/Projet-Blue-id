1e3c161efb7d1f91303a82b83d42881606eeeef6
@jordhylouzolo <156509693+louzolo123@users.noreply.github.com>
2024-01-16 15:09:33 +0100

    Update urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('main/', views.main , name='main'),
     path('personal/', views.list_personal_office , name='personal'),
     path('user/', views.list_users , name='user'),
-    path('add_user/', views.add_user , name='add_user'),
+    
+    
 ]
+
