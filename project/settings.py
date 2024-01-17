1e846741e203f29376b300223af1d82f818da003
Nox9 <129204433+Nox9900@users.noreply.github.com>
2024-01-17 17:25:07 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.mysql',
+    'ENGINE': 'django.db.backends.sqlite3',
     'NAME': 'blueid',
     'USER': 'root',
     'PASSWORD': '',
