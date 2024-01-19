97a4e0160ab0daad20a5250c955e2a2305f256de
Nox9 <129204433+Nox9900@users.noreply.github.com>
2024-01-19 16:13:34 +0100

    Update admin.py

--- apps/admin.py
+++ apps/admin.py
@@ -4,21 +4,17 @@ from .models import  PersonalOffice , Employed , User
 @admin.register(PersonalOffice)
 
 class PersonalOfficeAdmin(admin.ModelAdmin):
-    list_display =('name_personal', 'lastname_personal', 'sex_personal', 'phone_personal')
     class Meta:
         fields = '__all__'
         
 @admin.register(Employed)
 
 class EmployedAdmin(admin.ModelAdmin):
-    list_display = ('name_employed','lastname_employed','phone_employed','sex_employed','service_employed')
-    
     class Meta:
         fields = '__all__'
         
         
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display =('name_user', 'lastname_user', 'sex_user', 'phone_user', 'date_user','check_in_user','check_out_user')
     class Meta:
         fields = '__all__'
